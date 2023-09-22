9e82b925d628fb927e931d2a7d980783f806c5a5
lrl <742798240@qq.com>
2023-09-22 23:03:55 +0800

    fix: update

--- app/admin/layout.js
+++ app/admin/layout.js
@@ -1,4 +1,4 @@
-import AntdContainer from "components/AntdContainer";
+import AntdContainer from "@components/AntdContainer";
 
 export default function AdminContainer({ children }) {
 	return <AntdContainer>{children}</AntdContainer>;
