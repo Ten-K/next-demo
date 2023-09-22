aa0a850900415ac57af2434e06adc09c7eca72fa
lrl <742798240@qq.com>
2023-09-22 22:11:49 +0800

    fix: update

--- app/admin/(admin-layout)/layout.js
+++ app/admin/(admin-layout)/layout.js
@@ -1,4 +1,4 @@
-import AntAdmin from "components/AntAdmin";
+import AntAdmin from "@/app/admin/components/AntdContainer";
 
 export default function AdminLayout({ children }) {
 	return <AntAdmin>{children}</AntAdmin>;
